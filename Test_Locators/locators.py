7ad48d3f66d8fcb0d7a489214378c47a351c5aa5
Sheema parveen.B <141474798+Sheema-parveen@users.noreply.github.com>
2024-07-22 15:44:06 +0530

    All the locators are stored here

---
+++ Test_Locators/locators.py
@@ -0,0 +1,11 @@
+class Locators():
+    url='https://practicetestautomation.com/practice-test-login/'
+    username_id = "username" #id locator
+    password_id = "password" #id locator
+    submit_id = "submit" #id locator
+    sucess_url="https://practicetestautomation.com/logged-in-successfully/"
+    logout='//*[@id="loop-container"]/div/article/div[2]/div/div/div/a'
+
+    #utilities
+    file ="C:\\Users\\USER\\Desktop\\Datadriventesting\\Test_Data\\ddtf.xlsx"
+    sheet_no ='Sheet1'
